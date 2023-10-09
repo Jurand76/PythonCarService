fec5bf51c2fe1dd72061ab9dc5ad2704c573112c
Jurand76 <jurand@interia.pl>
2023-10-09 23:06:10 +0200

    modifying wydruk zlecenia

--- carserviceapp/wsgi.py
+++ carserviceapp/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for csharp project.
+WSGI config for carservice project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'csharp.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'carserviceapp.settings')
 
 application = get_wsgi_application()
